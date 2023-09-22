296560e5be4586d733ffc5ce9c8627f29ef88097
Joy Olago <joydanielle62@gmail.com>
2023-09-22 07:44:02 +0300

    completed assignment

---
+++ migrations/versions/82e9082b5964_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 82e9082b5964
+Revises: 
+Create Date: 2023-09-22 07:36:04.427788
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '82e9082b5964'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
